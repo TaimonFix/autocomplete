213be8dc35ef179f66f41c9c387d7847dbbaa167
BratyshevTD <bratyshev2013@mail.ru>
2022-12-28 09:27:01 +0300

    Доработки

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
-import java.util.*;
-import java.util.List;
-
+import java.util.Locale;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,23 +16,9 @@ public class Main {
 //        map.put("adafd", 1);
 //        System.out.println(map);
         PrefixTree root = new PrefixTree(' ');
-
         root.insert("fafa");
-
-
-        Map<String, Integer> extracted = new HashMap<>();
-
-        root.getAllStrings("", extracted);
-        System.out.println(extracted);
-
-        root.insert("bncg");
-        root.insert("fafa");
-        root.insert("fhbc");
-
-
-
-        root.getAllStrings("", extracted);
-        System.out.println(extracted);
-
+        root.insert("fafb");
+        root.insert("fafc");
+        System.out.println(root.containString("fafb"));
     }
 }
